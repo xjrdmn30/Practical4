ba9de07e931801d54ac9aad868eb79efb133e3df
xjrdmn30 <xjrdmn30@gmail.com>
2024-03-06 13:49:55 +0800

    variables committed

---
+++ ibi_practical/variables.py
@@ -0,0 +1,7 @@
+a=40
+b=36
+c=30
+d=a-b
+e=b-c
+print(d<e)
+#because the fact that d<e is true,we know that the improvement between b and c is larger,which means that training using	a	combination	of	running and strength exercises is greater
---
+++ studying_config/demo_1.py
@@ -0,0 +1,8 @@
+
+H=int(input('请输入你的身高'))
+M=int(input('请输入你的体重'))
+BMIa= H ** 2 / M
+input('你的BMI值为 print(BMIa)')
+
+
+
